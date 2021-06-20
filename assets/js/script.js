af22356fb45d4198e82374cb725080290e8d8df0
djkaz <justbekazu@gmail.com>
2021-06-20 09:48:36 -0500

    added the ability to operate offline

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 require("bootstrap");
 
+const img1 = require("../../assets/img/food-table.jpg");
+const img2 = require("../../assets/img/grill.jpg");
+
 $(document).ready(function () {
   // function createLoremIpsum(numWords = 50) {
   //   const loremIpsum =
@@ -46,14 +49,14 @@ $(document).ready(function () {
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
+      img: img1,
       btnText: "View Schedule",
       btnUrl: "schedule.html",
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
+      img: img2,
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html",
     },
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,31 @@ module.exports = {
     path: __dirname + "/dist",
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name(file) {
+                return "[path][name].[ext]";
+              },
+              publicPath: function (url) {
+                return url.replace("../", "/assets/");
+              },
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
+  },
+
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
